75320fcd879919b75d2807fc70421731b070a4b0
Jonathan Hogg <me@jonathanhogg.com>
2022-01-02 19:21:30 +0000

    Add quick'n'dirty support for profiling

--- flitter/__main__.py
+++ flitter/__main__.py
@@ -13,6 +13,7 @@ from .control import Controller
 parser = argparse.ArgumentParser(description="Flitter")
 parser.add_argument('--debug', action='store_true', default=False, help="Debug logging")
 parser.add_argument('--verbose', action='store_true', default=False, help="Informational logging")
+parser.add_argument('--profile', action='store_true', default=False, help="Run with profiling")
 parser.add_argument('script', nargs='+', help="Script to execute")
 args = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG if args.debug else (logging.INFO if args.verbose else logging.WARNING), stream=sys.stderr)
@@ -21,4 +22,8 @@ controller = Controller('.')
 for script in args.script:
     controller.load_page(script)
 controller.switch_to_page(0)
-asyncio.run(controller.run())
+if args.profile:
+    import cProfile
+    cProfile.run('asyncio.run(controller.run())', sort='tottime')
+else:
+    asyncio.run(controller.run())
