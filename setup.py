65f5c2206b7faec772a95906700c3672a1bbb66c
Jonathan Hogg <me@jonathanhogg.com>
2023-04-17 12:37:42 +0100

    Erm... oops! :roll_eyes:

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, Extension
 setup(
     ext_modules=[
         Extension("flitter.model", ["src/flitter/model.pyx"]),
-        Extension("flitter.langage.functions", ["src/flitter/language/functions.pyx"]),
-        Extension("flitter.langage.tree", ["src/flitter/language/tree.pyx"]),
+        Extension("flitter.language.functions", ["src/flitter/language/functions.pyx"]),
+        Extension("flitter.language.tree", ["src/flitter/language/tree.pyx"]),
         Extension("flitter.render.canvas", ["src/flitter/render/canvas.pyx"]),
         Extension("flitter.render.canvas3d", ["src/flitter/render/canvas3d.pyx"]),
         Extension("flitter.render.dmx", ["src/flitter/render/dmx.pyx"]),
