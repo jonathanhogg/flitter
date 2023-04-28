a0251806c0ba5046e2968ad48bee6f04c2cd1e47
Jonathan Hogg <me@jonathanhogg.com>
2023-04-28 08:26:49 +0100

    Fix build

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
         Extension("flitter.model", ["src/flitter/model.pyx"]),
         Extension("flitter.language.functions", ["src/flitter/language/functions.pyx"]),
         Extension("flitter.language.tree", ["src/flitter/language/tree.pyx"]),
-        Extension("flitter.render.canvas", ["src/flitter/render/canvas.pyx"]),
-        Extension("flitter.render.canvas3d", ["src/flitter/render/canvas3d.pyx"]),
+        Extension("flitter.render.window.canvas", ["src/flitter/render/window/canvas.pyx"]),
+        Extension("flitter.render.window.canvas3d", ["src/flitter/render/window/canvas3d.pyx"]),
         Extension("flitter.render.dmx", ["src/flitter/render/dmx.pyx"]),
         Extension("flitter.render.laser", ["src/flitter/render/laser.pyx"]),
     ]
