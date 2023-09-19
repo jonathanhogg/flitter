66127ceed32c0d19a7a5fb5b98c36cd49f8f4ee9
Jonathan Hogg <me@jonathanhogg.com>
2023-09-19 13:46:54 +0100

    Fix build

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, Extension
 
 setup(
     ext_modules=[
-        Extension("flitter.model", ["src/flitter/model.pyx"]),
-        Extension("flitter.interact.physics", ["src/flitter/interact/physics.pyx"]),
         Extension("flitter.language.functions", ["src/flitter/language/functions.pyx"]),
         Extension("flitter.language.noise", ["src/flitter/language/noise.pyx"]),
         Extension("flitter.language.tree", ["src/flitter/language/tree.pyx"]),
         Extension("flitter.language.vm", ["src/flitter/language/vm.pyx"]),
+        Extension("flitter.model", ["src/flitter/model.pyx"]),
+        Extension("flitter.render.dmx", ["src/flitter/render/dmx.pyx"]),
+        Extension("flitter.render.laser", ["src/flitter/render/laser.pyx"]),
+        Extension("flitter.render.physics", ["src/flitter/interact/physics.pyx"]),
         Extension("flitter.render.window.canvas", ["src/flitter/render/window/canvas.pyx"]),
         Extension("flitter.render.window.canvas3d", ["src/flitter/render/window/canvas3d.pyx"]),
         Extension("flitter.render.window.models", ["src/flitter/render/window/models.pyx"]),
-        Extension("flitter.render.dmx", ["src/flitter/render/dmx.pyx"]),
-        Extension("flitter.render.laser", ["src/flitter/render/laser.pyx"]),
     ]
 )
