0a2ec1a86595a2557fceb506d95b2213bec0e2f6
Jonathan Hogg <me@jonathanhogg.com>
2023-09-23 16:14:18 +0100

    Document API for renderers with a base class

--- src/flitter/render/__init__.py
+++ src/flitter/render/__init__.py
@@ -10,6 +10,38 @@ from loguru import logger
 _class_cache = {}
 
 
+class Renderer:
+    """
+    Renderers should implement this API
+    """
+    def __init__(self, **kwargs):
+        """
+        `kwargs` will contain the engine global definitions. You will likely
+        want to create most of the renderer state on first call of `update()`
+        when you have access to the top-level node attributes.
+        """
+        pass
+
+    def purge(self):
+        """
+        `purge()`` should wipe any local state ready for a complete redefinition.
+        """
+        raise NotImplementedError()
+
+    def destroy(self):
+        """
+        `destroy()`` should tear down the renderer itself.
+        """
+        raise NotImplementedError()
+
+    async def update(self, engine, node, **kwargs):
+        """
+        `update()`` is called every frame with the node to render and the engine
+        global definitions (including `clock` and `beat`).
+        """
+        raise NotImplementedError()
+
+
 def get_renderer(kind):
     if kind in _class_cache:
         return _class_cache[kind]
