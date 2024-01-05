1bce12a977119147ce99b045bdc9a5fd61ad239c
Jonathan Hogg <me@jonathanhogg.com>
2024-01-05 11:03:29 +0000

    Allow !image nodes to be resized
    This is only useful, really, if you want to down-size a large image 
    before using it as a texture to save GPU memory.

--- src/flitter/render/window/image.py
+++ src/flitter/render/window/image.py
@@ -2,6 +2,7 @@
 Video scene node
 """
 
+from loguru import logger
 import PIL.Image
 
 from . import SceneNode, COLOR_FORMATS
@@ -32,16 +33,26 @@ class Image(SceneNode):
         self.hidden = node.get('hidden', 1, bool, False)
         self.tags = node.tags
         filename = node.get('filename', 1, str)
-        if filename != self._filename:
+        if self._image is not None:
+            default_size = [self._image.width, self._image.height]
+        else:
+            default_size = None
+        size = node.get('size', 2, int, default_size)
+        if filename != self._filename or (size and size != [self.width, self.height]):
             self.release()
             self._filename = filename
+            self.width = None
+            self.height = None
         if self._filename is not None:
             image = SharedCache[filename].read_pil_image()
             if image is not self._image:
                 self._image = image
-                self.width = self._image.width
-                self.height = self._image.height
-                flipped = self._image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+                if size and size != [image.width, image.height]:
+                    image = image.resize(size, PIL.Image.Resampling.BILINEAR)
+                    logger.debug("Resized {} to {}x{}", self._filename, *size)
+                self.width = image.width
+                self.height = image.height
+                flipped = image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
                 if self._image.has_transparency_data:
                     self._texture = self.glctx.texture((self.width, self.height), 4, internal_format=GL_SRGB8)
                     self._texture.write(flipped.convert('RGBA').convert('RGBa').tobytes())
