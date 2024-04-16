e8fdde84e25d4a107044f1ae2c7c062684e66d72
Jonathan Hogg <me@jonathanhogg.com>
2024-04-16 13:07:28 +0100

    Don't use power operator in quad and cubic easing functions

--- src/flitter/language/functions.pyx
+++ src/flitter/language/functions.pyx
@@ -451,14 +451,19 @@ def quad(Vector xs not None):
     if xs.numbers == NULL:
         return null_
     cdef Vector ys = Vector.__new__(Vector)
-    cdef double x, y
+    cdef double x, y, x_
     for i in range(ys.allocate_numbers(xs.length)):
         x = xs.numbers[i]
         if x < 0:
-            x = 0
+            y = 0
         elif x > 1:
-            x = 1
-        y = (x * 2)**2 / 2 if x < 0.5 else 1 - ((1 - x) * 2)**2 / 2
+            y = 1
+        elif x < 0.5:
+            x_ = x * 2
+            y = x_*x_ / 2
+        else:
+            x_ = (1 - x) * 2
+            y = 1 - x_*x_ / 2
         ys.numbers[i] = y
     return ys
 
@@ -467,14 +472,19 @@ def cubic(Vector xs not None):
     if xs.numbers == NULL:
         return null_
     cdef Vector ys = Vector.__new__(Vector)
-    cdef double x, y
+    cdef double x, y, x_
     for i in range(ys.allocate_numbers(xs.length)):
         x = xs.numbers[i]
         if x < 0:
-            x = 0
+            y = 0
         elif x > 1:
-            x = 1
-        y = (x * 2)**3 / 2 if x < 0.5 else 1 - ((1 - x) * 2)**3 / 2
+            y = 1
+        elif x < 0.5:
+            x_ = x * 2
+            y = x_*x_*x_ / 2
+        else:
+            x_ = (1 - x) * 2
+            y = 1 - x_*x_*x_ / 2
         ys.numbers[i] = y
     return ys
 
