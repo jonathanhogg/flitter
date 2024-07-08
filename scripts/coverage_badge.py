e60fa158c8ceca6865a3ee51f87b5e62f3dc72c8
Jonathan Hogg <me@jonathanhogg.com>
2024-07-08 18:06:38 +0100

    Tweak the coverage badge

---
+++ scripts/coverage_badge.py
@@ -0,0 +1,22 @@
+"""
+Extract coverage information for making the README badge
+"""
+
+import json
+
+with open('coverage.json') as f:
+    data = json.load(f)
+    percent = data['totals']['percent_covered']
+    print(f'status={percent:.0f}%')
+    if percent > 90:
+        print('color=blue')
+    if percent > 80:
+        print('color=cyan')
+    elif percent > 70:
+        print('color=green')
+    elif percent > 60:
+        print('color=yellow')
+    elif percent > 50:
+        print('color=orange')
+    else:
+        print('color=red')
