129c8200dcc3920d0317c73cc126c149847586d9
Jonathan Hogg <me@jonathanhogg.com>
2024-07-09 18:52:28 +0100

    Calculate the coverage badge colour

--- scripts/coverage_badge.py
+++ scripts/coverage_badge.py
@@ -2,21 +2,13 @@
 Extract coverage information for making the README badge
 """
 
+import colorsys
 import json
 
 with open('coverage.json') as f:
     data = json.load(f)
     percent = data['totals']['percent_covered']
+    r, g, b = colorsys.hsv_to_rgb(max(0, percent/100-0.5)*2/3, 0.9, 0.7)
+    color = (int(round(r*16)) << 8) + (int(round(g*16)) << 4) + int(round(b*16))
     print(f'status={percent:.0f}%')
-    if percent > 90:
-        print('color=blue')
-    if percent > 80:
-        print('color=cyan')
-    elif percent > 70:
-        print('color=green')
-    elif percent > 60:
-        print('color=BA1')
-    elif percent > 50:
-        print('color=orange')
-    else:
-        print('color=red')
+    print(f'color={color:03X}')
