93af022e957d0dacc7d293196580d64780550898
Jonathan Hogg <me@jonathanhogg.com>
2024-07-16 13:51:28 +0100

    Fix incorrect `!noise` `components` default

--- src/flitter/render/window/__init__.py
+++ src/flitter/render/window/__init__.py
@@ -439,7 +439,7 @@ class Noise(Shader):
 
     def render(self, node, **kwargs):
         seed_hash = hash(node['seed'] if 'seed' in node else null) / (1 << 48)
-        super().render(node, seed_hash=seed_hash, components=0, octaves=1, roughness=0.5, origin=0, z=0, scale=1, tscale=1, **kwargs)
+        super().render(node, seed_hash=seed_hash, components=1, octaves=1, roughness=0.5, origin=0, z=0, scale=1, tscale=1, **kwargs)
 
 
 class GLFWLoader:
