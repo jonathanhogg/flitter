8ea58ac0bd11e67480acc2edd6822524d5af6485
Jonathan Hogg <me@jonathanhogg.com>
2024-09-07 19:00:36 +0100

    Update version number for b18 release

--- src/flitter/__init__.py
+++ src/flitter/__init__.py
@@ -20,7 +20,7 @@ except ImportError:
         pass
 
 
-__version__ = "1.0.0b17"
+__version__ = "1.0.0b18"
 
 LOGGING_LEVEL = "SUCCESS"
 LOGGING_FORMAT = "{time:HH:mm:ss.SSS} {process}:{extra[shortname]:16s} | <level>{level}: {message}</level>"
