4a7c4dad28a4df7642fde34228b621a895670874
Jonathan Hogg <me@jonathanhogg.com>
2024-12-22 14:39:11 +0000

    Fix bug in nearest pixel sampling
    Enabling it would disable border settings.

--- src/flitter/render/window/__init__.py
+++ src/flitter/render/window/__init__.py
@@ -279,7 +279,7 @@ class ProgramNode(WindowNode):
         else:
             sampler_args = {'border_color': (0, 0, 0, 0)}
         if node.get('nearest', 1, bool, False):
-            sampler_args = {'filter': (moderngl.NEAREST, moderngl.NEAREST)}
+            sampler_args['filter'] = moderngl.NEAREST, moderngl.NEAREST
         child_textures = self.child_textures
         samplers = []
         self.glctx.extra['zero'].use(0)
